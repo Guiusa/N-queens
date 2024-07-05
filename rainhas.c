8187fb228845bba8119d30a54a2f663722f7f0be
guiusa <guiusepeoneda@gmail.com>
2024-07-05 16:12:06 -0300

    Adicionado destruição do grafo

--- rainhas.c
+++ rainhas.c
@@ -4,6 +4,8 @@
 #include <string.h>
 #define uint unsigned int
 
+
+
 /*
  * Checa se a posição está proibida
  */
@@ -28,6 +30,7 @@ static int *map(casa *c, int k, unsigned int n){
     return fbd ;
 }
 //##############################################################################
+
 
 
 /*
@@ -92,121 +95,6 @@ static unsigned int *rainhas_bt_wrapped(unsigned int n, unsigned int *r, int ll,
 //##############################################################################
 
 
-/*
- * Struct da fila
- */
-typedef struct queue_t {
-    struct queue_t *next ;
-    struct nodo_t *points ;
-} queue_t ;
-
-/*
-* Struct do nodo
-*/
-typedef struct nodo_t {
-    struct queue_t *adj ;
-    uint l, c ;
-    short valid, percorrido ;
-} nodo_t;
-
-
-static void print_fila(nodo_t* n){
-    queue_t *aux = n->adj ;
-    if(!aux) return ;
-    while(aux){
-        printf("->[%d][%d]", aux->points->l, aux->points->c) ;
-        aux = aux->next ;
-    }
-}
-
-/*
- * Printa o grafo
- */
-static void print_graph(nodo_t *g, int n){
-    for(int i = 0; i<n*n; i++){
-        printf("[%d][%d]", g[i].l, g[i].c) ;
-        print_fila(&g[i]) ;
-        printf("\n") ;
-    }
-}
-//##############################################################################
-
-
-
-static nodo_t *init_nodo_t(nodo_t* nodo, uint l, uint c) {
-    nodo->l = l ;
-    nodo->c = c ;
-    nodo->valid = 1 ;
-    nodo->percorrido = 0 ;
-    nodo->adj = NULL ;
-    return nodo ;
-}
-
-static queue_t *init_queue_t(nodo_t* points){
-    queue_t* queue_node = (queue_t *) malloc(sizeof(queue_t)) ;
-    queue_node->points = points ;
-    queue_node->next = NULL ;
-    return queue_node ;
-}
-static void queue_append(nodo_t* head, queue_t* elem){
-    elem->next = head->adj ;
-    head->adj = elem ;
-}
-
-
-/*
- * Adiciona casas atacadas de acordo com a distância
- * Retorna número de arestas criadas
- */
-static int add_by_distance(int i, int j, nodo_t* g, int d, int n){
-    int edges = 0 ;
-
-    for(int iaux = -d; iaux<= +d; iaux+=d){
-        if(i+iaux < 0 || i+iaux >= n) continue ;
-        for(int jaux = -d; jaux <= d; jaux+=d){
-            if(j+jaux < 0 || j+jaux >= n) continue ;
-            if(jaux == 0 && iaux == 0) continue ;
-            nodo_t* nodo_aux1 = &g[(i*n) + j] ;
-            nodo_t* nodo_aux2 = &g[((i+iaux)*n) + (j+jaux)] ;
-            if(nodo_aux2->percorrido) continue ;
-
-            queue_t* queue_aux1 = init_queue_t(nodo_aux1) ;
-            queue_t* queue_aux2 = init_queue_t(nodo_aux2) ;
-
-            queue_append(nodo_aux1, queue_aux2) ;
-            queue_append(nodo_aux2, queue_aux1) ;
-            edges++ ;
-        }
-    }
-    g[i*n + j].percorrido = 1 ;
-    return edges ;
-}
-//##############################################################################
-
-
-/*
- * Cria uma representação do tabuleiro com uma lista de adjacência
- */
-static nodo_t* init_graph(int n){
-    nodo_t *graph = (nodo_t *) malloc(n*n * sizeof(nodo_t)) ;
-    // Inicia todos os nodos do vetor principal
-    for(int i = 0; i<n*n; i++){
-        nodo_t* aux = &graph[i] ;
-        init_nodo_t(aux, i/n, i%n) ;
-    }
-
-    for(int i = 0; i<n; i++){
-        for(int j = 0; j<n; j++){
-            for(int d = 1; d < n; d++){
-                add_by_distance(i, j, graph, d, n) ;
-            }
-        }
-    }
-
-    return graph ;
-}
-//##############################################################################
-
 
 //------------------------------------------------------------------------------
 // computa uma resposta para a instância (n,c) do problema das n rainhas 
@@ -238,6 +126,170 @@ unsigned int *rainhas_bt(unsigned int n, unsigned int k, casa *c, unsigned int *
 
 
 
+/*
+ * Struct da fila
+ */
+typedef struct queue_t {
+    struct queue_t *next ;
+    struct nodo_t *points ;
+} queue_t ;
+/*
+* Struct do nodo
+*/
+typedef struct nodo_t {
+    struct queue_t *adj ;
+    uint l, c ;
+    short valid, percorrido ;
+} nodo_t ;
+
+
+
+/*
+ * Printa os elementos enfileirados nas filas de adjacência
+ */
+static void print_fila(nodo_t* n){
+    queue_t *aux = n->adj ;
+    if(!aux) return ;
+    while(aux){
+        printf("->[%d][%d]", aux->points->l, aux->points->c) ;
+        aux = aux->next ;
+    }
+}
+//##############################################################################
+
+
+
+/*
+ * Printa o grafo
+ */
+static void print_graph(nodo_t *g, int n){
+    for(int i = 0; i<n*n; i++){
+        printf("[%d][%d]", g[i].l, g[i].c) ;
+        print_fila(&g[i]) ;
+        printf("\n") ;
+    }
+}
+//##############################################################################
+
+
+
+/*
+ * Inicializa um nodo no vetor principal do grafo
+ */
+static nodo_t *init_nodo_t(nodo_t* nodo, uint l, uint c) {
+    nodo->l = l ;
+    nodo->c = c ;
+    nodo->valid = 1 ;
+    nodo->percorrido = 0 ;
+    nodo->adj = NULL ;
+    return nodo ;
+}
+//##############################################################################
+
+
+
+/*
+ * Inicializa um elemento a ser adicionado nas listas de adjacência
+ */
+static queue_t *init_queue_t(nodo_t* points){
+    queue_t* queue_node = (queue_t *) malloc(sizeof(queue_t)) ;
+    queue_node->points = points ;
+    queue_node->next = NULL ;
+    return queue_node ;
+}
+//##############################################################################
+
+
+
+/*
+ * Adiciona um elemento à lista (adiciona no inicio para não percorrer)
+ */
+static void queue_append(nodo_t* head, queue_t* elem){
+    elem->next = head->adj ;
+    head->adj = elem ;
+}
+//##############################################################################
+
+
+
+/*
+ * Adiciona casas atacadas de acordo com a distância
+ * Retorna número de arestas criadas
+ * Se d=1, adiciona todas as casas que atacam a casa [i][j] e estão a 1 casa de
+ * distância
+ */
+static int add_by_distance(int i, int j, nodo_t* g, int d, int n){
+    int edges = 0 ;
+    nodo_t* nodo_aux1 = &g[(i*n) + j] ;
+    
+    for(int iaux = -d; iaux<= +d; iaux+=d){
+        // Se a linha ultrapassa os limites do tabuleiro, continua
+        if(i+iaux < 0 || i+iaux >= n) continue ;
+
+        for(int jaux = -d; jaux <= d; jaux+=d){
+            // Se a coluna ultrapassa so limites do tabuleiro, continua
+            if(j+jaux < 0 || j+jaux >= n) continue ;
+            // Se a casa comparada for a própria casa recebida, não adiciona
+            if(jaux == 0 && iaux == 0) continue ;
+
+            nodo_t* nodo_aux2 = &g[((i+iaux)*n) + (j+jaux)] ;
+            // Se a casa a ser comparada já foi percorrida, não insere de novo
+            if(nodo_aux2->percorrido) continue ;
+
+            queue_t* queue_aux1 = init_queue_t(nodo_aux1) ;
+            queue_t* queue_aux2 = init_queue_t(nodo_aux2) ;
+
+            // X é vizinho de Y, então X é adicionado à lista de Y e vice-versa
+            queue_append(nodo_aux1, queue_aux2) ;
+            queue_append(nodo_aux2, queue_aux1) ;
+            edges++ ;
+        }
+    }
+    nodo_aux1->percorrido = 1 ;
+    return edges ;
+}
+//##############################################################################
+
+
+/*
+ * Cria uma representação do tabuleiro com uma lista de adjacência
+ */
+static nodo_t* init_graph(int n){
+    nodo_t *graph = (nodo_t *) malloc(n*n * sizeof(nodo_t)) ;
+    // Inicia todos os nodos do vetor principal
+    for(int i = 0; i<n*n; i++){
+        nodo_t* aux = &graph[i] ;
+        init_nodo_t(aux, i/n, i%n) ;
+    }
+
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<n; j++){
+            for(int d = 1; d < n; d++){
+                add_by_distance(i, j, graph, d, n) ;
+            }
+        }
+    }
+
+    return graph ;
+}
+//##############################################################################
+
+
+
+/*
+ * Libera o grafo e inibe leaks
+ */
+static void destroy_graph(nodo_t *g, int n){
+    for(int i = 0; i<n*n; i++)
+        while(g[i].adj){
+            queue_t *aux = g[i].adj ;
+            g[i].adj = g[i].adj->next ;
+            free(aux) ;
+        }
+    free(g) ;
+}
+//##############################################################################
+
 //------------------------------------------------------------------------------
 // computa uma resposta para a instância (n,c) do problema das n
 // rainhas com casas proibidas usando a modelagem do problema como
@@ -247,5 +299,6 @@ unsigned int *rainhas_bt(unsigned int n, unsigned int k, casa *c, unsigned int *
 unsigned int *rainhas_ci(unsigned int n, unsigned int k, casa *c, unsigned int *r) {
     nodo_t* graph = init_graph((int) n) ;
     print_graph(graph, (int) n) ;
+    destroy_graph(graph, (int) n) ;
     return r;
 }
